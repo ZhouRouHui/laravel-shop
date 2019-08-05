f1ad5de21007760a4fadfee9766138c09326c99d
loedan <loedan@163.com>
2019-08-05 10:09:56 +0000

    购物车页面

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -4,9 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\AddCartRequest;
 use App\Models\CartItem;
+use App\Models\ProductSku;
+use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
+    /**
+     * 添加购物车
+     *
+     * @param AddCartRequest $request
+     * @return array
+     */
     public function add(AddCartRequest $request)
     {
         $user   = $request->user();
@@ -28,6 +36,40 @@ class CartController extends Controller
             $cart->productSku()->associate($skuId);
             $cart->save();
         }
+
+        return [];
+    }
+
+    /**
+     * 购物车列表
+     *
+     * @param Request $request
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function index(Request $request)
+    {
+        /**
+         * with(['productSku.product']) 方法用来预加载购物车里的商品和 SKU 信息。
+         * 如果这里没有进行预加载而是在渲染模板时通过 $item->productSku->product 这种懒加载的方式，
+         * 就会出现购物车中的每一项都要执行多次商品信息的 SQL 查询，导致单个页面执行的 SQL 数量过多，
+         * 加载性能差的问题，也就是经典的 N + 1 查询问题。使用了预加载之后，
+         * Laravel 会通过类似 select * from product_skus where id in (xxxx) 的方式把原本需要多条 SQL 查询的数据用一条 SQL 就查到了，
+         * 大大提升了执行效率。同时 Laravel 还支持通过 . 的方式加载多层级的关联关系，这里我们就通过 . 提前加载了与商品 SKU 关联的商品。
+         */
+        $cartItems = $request->user()->cartItems()->with(['productSku.product'])->get();
+
+        return view('cart.index', ['cartItems' => $cartItems]);
+    }
+
+    /**
+     * 删除购物车商品
+     * @param ProductSku $sku
+     * @param Request $request
+     * @return array
+     */
+    public function remove(ProductSku $sku, Request $request)
+    {
+        $request->user()->cartItems()->where('product_sku_id', $sku->id)->delete();
 
         return [];
     }
--- routes/web.php
+++ routes/web.php
@@ -36,6 +36,8 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
 
     // 购物车
     Route::post('cart', 'CartController@add')->name('cart.add');
+    Route::get('cart', 'CartController@index')->name('cart.index');
+    Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
 });
 
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
