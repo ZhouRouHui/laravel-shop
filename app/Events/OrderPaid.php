e806b89635cfc21cc3d871eb91fd2c47a18c2449
loedan <loedan@163.com>
2019-08-06 08:38:24 +0000

    完善支付后逻辑

---
+++ app/Events/OrderPaid.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Order;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class OrderPaid
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * 订单
+     * @var Order $order
+     */
+    protected $order;
+
+    /**
+     * Create a new event instance.
+     *
+     * OrderPaid constructor.
+     * @param Order $order
+     */
+    public function __construct(Order $order)
+    {
+        $this->order = $order;
+    }
+
+    /**
+     * @return Order
+     */
+    public function getOrder()
+    {
+        return $this->order;
+    }
+}
