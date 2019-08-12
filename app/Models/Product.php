3399852d6059957237a2aaf8157c212d7b94a93e
loedan <loedan@163.com>
2019-08-12 09:18:21 +0000

    管理后台商品控制器封装

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -11,7 +11,7 @@ class Product extends Model
     const TYPE_NORMAL = 'normal';
     const TYPE_CROWDFUNDING = 'crowdfunding';
 
-    protected static $typeMap = [
+    public static $typeMap = [
         self::TYPE_NORMAL => '普通商品',
         self::TYPE_CROWDFUNDING => '众筹商品'
     ];
